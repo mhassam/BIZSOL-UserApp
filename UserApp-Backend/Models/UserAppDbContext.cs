3d0bb82fea66745bfa5dfe566a2914ea0e055127
hassam-atrule <muhammad.hassam@atrule.com>
2023-05-17 07:35:27 +0500

    FIrst Check-In
    -Replace Urls in js and controller with current one.

---
+++ UserApp-Backend/Models/UserAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserApp_Backend.Models
+{
+    public class UserAppDbContext : DbContext
+    {
+        public UserAppDbContext(DbContextOptions contextOptions) : base(contextOptions)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
